05174a591c144906d854950164ff0417fba7b2dd
Kamlesh Kirodewal <kkirodewal@gmail.com>
2021-04-12 09:31:48 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -81,3 +81,16 @@ async def upgrade(bot, update):
         reply_to_message_id=update.message_id,
         disable_web_page_preview=True
     )
+
+
+@pyrogram.Client.on_message(pyrogram.filters.command(["source"]))
+async def help_user(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/source")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.SOURCE,
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_to_message_id=update.message_id
+    )
