416d0773aedc9fbeccfdb6436ab9ec80d46ec33d
Kamlesh Kirodewal <kkirodewal@gmail.com>
2021-04-12 07:15:25 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -33,13 +33,6 @@ from helper_funcs.help_uploadbot import DownLoadFile
 
 @pyrogram.Client.on_message(pyrogram.filters.regex(pattern=".*http.*"))
 async def echo(bot, update):
-    if update.from_user.id not in Config.AUTH_USERS:
-        await bot.delete_messages(
-            chat_id=update.chat.id,
-            message_ids=update.message_id,
-            revoke=True
-        )
-        return
     # logger.info(update)
     TRChatBase(update.from_user.id, update.text, "/echo")
     # await bot.send_chat_action(
