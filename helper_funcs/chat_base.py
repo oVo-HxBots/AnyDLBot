2c6d32c28105d1f6436129884f0e28469a8af23c
Rachel Saini <Css@kirodewal.eu.org>
2022-09-10 09:29:42 +0530

    Update chat_base.py

--- helper_funcs/chat_base.py
+++ helper_funcs/chat_base.py
@@ -19,7 +19,7 @@ else:
 # the Strings used for this "thing"
 from translation import Translation
 
-from pyrogram import Client, filters
+from pyrogram import Client, Filters as filters
 
 # the Telegram trackings
 from chatbase import Message
