09e93646fc2b8a10fcfaffdea387499dd06b438f
Kamlesh Saini <88968064+oVo-HxBots@users.noreply.github.com>
2023-09-11 16:49:49 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -40,7 +40,7 @@ async def help_user(bot, update):
         text=Translation.HELP_USER,
         parse_mode="html",
         disable_web_page_preview=True,
-        reply_to_message_id=update.message_id
+        reply_to_message_id=message_id
     )
 
 
@@ -65,7 +65,7 @@ async def start(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.START_TEXT,
-        reply_to_message_id=update.message_id
+        reply_to_message_id=message_id
     )
 
 
