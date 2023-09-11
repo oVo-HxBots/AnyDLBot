5d66cc73b1f0da77fc1ddfcb46fe145e909541cf
Kamlesh Saini <88968064+oVo-HxBots@users.noreply.github.com>
2023-09-11 16:02:00 +0530

    Update convert_to_audio.py

--- plugins/convert_to_audio.py
+++ plugins/convert_to_audio.py
@@ -32,7 +32,7 @@ from hachoir.parser import createParser
 from PIL import Image
 
 
-@pyrogram.Client.on_message(pyrogram.Filters.command(["converttoaudio"]))
+@pyrogram.Client.on_message(pyrogram.filters.command(["converttoaudio"]))
 async def convert_to_audio(bot, update):
     TRChatBase(update.from_user.id, update.text, "converttoaudio")
     if (update.reply_to_message is not None) and (update.reply_to_message.media is not None) :
