21e6f2d86cca5d3e5b7180cee27fb21cd984da8d
Kamlesh Saini <88968064+oVo-HxBots@users.noreply.github.com>
2023-09-11 16:59:47 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -65,7 +65,7 @@ async def start(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.START_TEXT,
-        reply_to_message_id=message.id
+        reply_to_message_id=update.message.id
     )
 
 
